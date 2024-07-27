be54c8f35222be65dff4765f01dfe30f7bb45f6e
Zharks <nevraddacunha@myges.fr>
2024-07-28 01:05:54 +0200

    feat: login with jwt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
     return (
@@ -7,6 +8,7 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
         </Router>
