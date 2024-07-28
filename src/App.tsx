4ca8b49e4f7234fe6d6d42515e23a6387c81101d
Zharks <nevraddacunha@myges.fr>
2024-07-28 19:59:36 +0200

    feat: dashboard + bugged user edition
    Modification for personal information are not registered if we also modify the password because the "edit password" has a higher priority => need to fix that by uncoupling them

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Dashboard from "./pages/Dashboard.tsx";
 
 function App() {
     return (
@@ -9,6 +10,8 @@ function App() {
                 <Routes>
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/" element={<Login />} />
                 </Routes>
             </div>
         </Router>
