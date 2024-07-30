a3d463aaee7eb0480e2a1a60ad692698afad592c
Zharks <nevraddacunha@myges.fr>
2024-07-30 18:12:04 +0200

    feat: header dynamic content for multiple user

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from "./pages/Dashboard.tsx";
+import EstimateLocation from "./pages/EstimateLocation.tsx";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/" element={<Login />} />
+                    <Route path="/estimate" element={<EstimateLocation />} />
                 </Routes>
             </div>
         </Router>
