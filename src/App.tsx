58810ac5570ea6ac7287d94746db95bfa5868aeb
Zharks <nevraddacunha@myges.fr>
2024-07-31 20:29:51 +0200

    feat: stripe yearly subscription for location

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import Dashboard from "./pages/Dashboard.tsx";
-import EstimateLocation from "./pages/EstimateLocation.tsx";
+import Dashboard from "./pages/Dashboard";
+import EstimateLocation from "./pages/EstimateLocation";
+import SuccesPageRenter from "./pages/SuccessPageRenter.tsx";
+import CanceledPageRenter from "./pages/CanceledPageRenter.tsx";
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/" element={<Login />} />
                     <Route path="/estimate" element={<EstimateLocation />} />
+                    <Route path="/success" element={<SuccesPageRenter />} />
+                    <Route path="/canceled" element={<CanceledPageRenter />} />
                 </Routes>
             </div>
         </Router>
