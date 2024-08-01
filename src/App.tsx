e8c2d28610d8c778e6688e01f8c1d5f082d07f03
Zharks <nevraddacunha@myges.fr>
2024-08-01 19:07:07 +0200

    feat: before publishing + upload paper asset

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from "./pages/Dashboard";
 import EstimateLocation from "./pages/EstimateLocation";
-import SuccessPageRenter from "./pages/SuccessPageRenter.tsx";
+import SuccesPageRenter from "./pages/SuccessPageRenter.tsx";
 import CanceledPageRenter from "./pages/CanceledPageRenter.tsx";
 import AdminLocations from "./pages/AdminLocations";
+import AdminLocationsReview from "./pages/AdminLocationsReview.ts.tsx";
 
 function App() {
     return (
@@ -17,9 +18,10 @@ function App() {
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/" element={<Login />} />
                     <Route path="/estimate" element={<EstimateLocation />} />
-                    <Route path="/success" element={<SuccessPageRenter />} />
+                    <Route path="/success" element={<SuccesPageRenter />} />
                     <Route path="/canceled" element={<CanceledPageRenter />} />
                     <Route path="/admin/locations" element={<AdminLocations />} />
+                    <Route path="/admin/locations/:locationId/review" element={<AdminLocationsReview />} />
                 </Routes>
             </div>
         </Router>
